3112690eb9d632b7acfbea95647aab1042ca762c
Phil de Grandi <phil.degrandi@btc-ag.com>
2022-05-25 12:44:39 +0200

    Initial commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,45 @@
+import os
+import sqlite3
+from flask import Flask, render_template, current_app, g
+import flask_login
+import click
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+    login_manager = flask_login.LoginManager()
+    login_manager.init_app(app)
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/')
+    def hello():
+        return render_template()
+
+    return app
